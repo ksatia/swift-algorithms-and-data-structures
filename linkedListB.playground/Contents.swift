ca4a83303ebb2faefe9c26a3bb9ec1260a1b7dc6
ksatia <ksatia@gmail.com>
2016-05-19 16:44:34 -0400

    fix removeItemAtIndex func for tail removal logic

--- linkedListB.playground/Contents.swift
+++ linkedListB.playground/Contents.swift
@@ -49,19 +49,14 @@ class LinkedList<T:Equatable> {
             if self.count != 1 { //this means there are at least 2 elements in the linked list. Int (position) can be 0 at the lowest. Count is greater than 0, and if it's not 1, it
                 //must be 2 or greater.                /
                 if position == 0 { //item to remove is the head so we must remove connections to the next one, and make the next one the head.
-                    let newHead = self.head.next!
-                    self.head = newHead //pointers to further nodes are maintained, we simply dropped out the current head and reassigned the next one
+                    //let newHead = self.head.next!
+                    self.head = self.head.next! //pointers to further nodes are maintained, we simply dropped out the current head and reassigned the next one
                 }
                 if position == self.count-1 { //item to remove is the tail so we must get the previous node and make that the new tail.
-                    /*
-                    Code below plays around with implem. for circular doubly ll. Make the original tail's previous node point to the original tails next node, and make the original tail's previous node be the new tail. We could also just have self.tail.prev!.next = self.head, as tail->next should == self.head
-                    self.tail.prev!.next = tail.next
-                    self.tail = tail.prev!
-                    */
-                    let newTail = self.tail.prev!
-                    self.tail = newTail
+                    self.tail.prev!.next = nil //tail points to nothing, so have tail->previous have the next node be nil
+                    self.tail = self.tail.prev!
                 }
-                else {
+                if (position > 0 && position < count-1) {
                     var currentNode = self.head
                     for _ in 0...position-1 {
                         currentNode = currentNode.next!
@@ -74,7 +69,7 @@ class LinkedList<T:Equatable> {
             if self.count == 1 { //we have a single element and we're now removing it. We won't set linked list to nil, but rather give it an initialized head that has no
                 //data for next, prev or value properties
                 self.head = Node<T>()
-                //self.tail = Node<T>()        if we were making this circular, then we would also have a blank node for the tail, as one node can be both a head and tail
+                self.tail = Node<T>()        //if we were making this circular, then we would also have a blank node for the tail, as one node can be both a head and tail
             }
             self.count-- //always maintain the element count as our operations rely on it for figuring out computations.
         }
@@ -98,11 +93,12 @@ class LinkedList<T:Equatable> {
 
 var list = LinkedList<String>()
 list.isEmpty()
-list.addItem("hi")
-list.addItem("you")
-list.addItem("idiot")
+list.addItem("this")
+list.addItem("is a")
+list.addItem("linked list")
+list.addItem("in swift")
 
 list.printList()
-list.removeItemAtIndex(1)
+list.removeItemAtIndex(3)
 list.printList()
 
