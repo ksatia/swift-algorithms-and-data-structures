558d10ebdbaae6041b02f6d97a157eed78b3ef57
ksatia <ksatia@gmail.com>
2016-05-31 15:36:33 -0400

    Add comments

--- Data Structures/SelectionSort.swift
+++ Data Structures/SelectionSort.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class SelectionSort: NSObject {
 
-    // we must pass the address of the array to our inout swap function. aka swap(&array, 2, 5)
+    // simple swap function using reassignment instead of memory swapping
     func swap (inout array: [Int], firstIndex: Int, secondIndex:Int) {
         let temp = array[firstIndex]
         array[firstIndex] = array[secondIndex]
@@ -20,12 +20,16 @@ class SelectionSort: NSObject {
     func selectionSort (inout array: [Int]) -> Array<Int> {
         var minimumElement: Int
         for n in 0..<array.count { // loops from 0 to n-1
+            // set minimum element equal to starting index being used for iteration
             minimumElement = n
+            // iterate over the remaining elements
             for x in n+1..<array.count {
+                // if any of the elements being iterated over are less than our minimum, reassign the minimum index
                 if array[x] < array[minimumElement] {
                     minimumElement = x
                 }
             }
+            // if our new minimum if different from the first element in the unsorted portion of our array, swap them
             if minimumElement != n {
                 swap(&array, firstIndex: n, secondIndex: minimumElement)
             }
